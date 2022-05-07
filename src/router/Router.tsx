b3af007783687e9634587eb22b5df6fa60eaff82
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-05-07 15:10:37 -0300

    Drawer style

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import {Button, View} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {shade} from 'polished';
 import {HomeScreen} from '../screens/HomeScreen/HomeScreen';
+import {colors} from '../styles/colors';
 
 function NotificationsScreen({navigation}) {
   return (
@@ -15,8 +17,21 @@ const Drawer = createDrawerNavigator();
 
 export default function Router() {
   return (
-    <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home" component={HomeScreen} />
+    <Drawer.Navigator
+      screenOptions={{
+        headerTintColor: colors.primary,
+
+        headerStyle: {
+          backgroundColor: colors.secondary,
+        },
+        drawerStyle: {backgroundColor: colors.background},
+        drawerItemStyle: {
+          backgroundColor: shade(0.6, colors.primary),
+        },
+        drawerLabelStyle: {color: colors.primary},
+      }}
+      initialRouteName="TVmaze">
+      <Drawer.Screen name="TVmaze" component={HomeScreen} />
       <Drawer.Screen name="Notifications" component={NotificationsScreen} />
     </Drawer.Navigator>
   );
