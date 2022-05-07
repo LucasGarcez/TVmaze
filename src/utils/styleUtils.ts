1246bf7121c119983bb234850c661380b1da750b
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-05-07 12:26:55 -0300

    shadow utils

---
+++ src/utils/styleUtils.ts
@@ -0,0 +1,13 @@
+const shadow = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 3,
+  },
+  shadowOpacity: 0.27,
+  shadowRadius: 4.65,
+
+  elevation: 6,
+};
+
+export const stylesUtils = {shadow};
