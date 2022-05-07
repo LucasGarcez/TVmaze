286732a827b578a637acde7c7023a21a9cd4f750
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-05-07 15:08:56 -0300

    showService searchByName

--- src/services/show/showService.ts
+++ src/services/show/showService.ts
@@ -1,11 +1,26 @@
 import {Show} from '../../models/ShowModel';
 import {api} from '../api';
 
-async function list(search?: string): Promise<Show[]> {
+async function list(): Promise<Show[]> {
   const {data} = await api.get<Show[]>('shows');
   return data;
 }
 
+interface SearchShowResult {
+  score: number;
+  show: Show;
+}
+async function searchByName(searchText: string): Promise<Show[]> {
+  if (searchByName.length < 1) return [];
+  const {data} = await api.get<SearchShowResult[]>(
+    `search/shows/?q=${searchText}`,
+  );
+
+  const showList = data.map(value => value.show);
+  return showList;
+}
+
 export const showService = {
   list,
+  searchByName,
 };
