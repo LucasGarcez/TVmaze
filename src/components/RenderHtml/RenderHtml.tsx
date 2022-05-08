5fe30ed51b5ac0817013dd557d7c103b39bc16b5
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-05-08 16:58:55 -0300

    RenderHtml componente

---
+++ src/components/RenderHtml/RenderHtml.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {useWindowDimensions} from 'react-native';
+import RenderHtmlRN from 'react-native-render-html';
+import {colors} from '../../styles/colors';
+import {SIZE} from '../../utils/constants';
+
+interface Props {
+  html: string;
+}
+export function RenderHtml({html}: Props) {
+  const {width} = useWindowDimensions();
+  const CONTENT_WIDTH = width - SIZE.padding * 2;
+  return (
+    <RenderHtmlRN
+      baseStyle={{color: colors.onBackground}}
+      contentWidth={CONTENT_WIDTH}
+      source={{html}}
+    />
+  );
+}
