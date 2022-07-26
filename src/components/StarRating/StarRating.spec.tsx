c615939783cb57649fbdecc87490abb28e55f864
Arllan Pablo <arllanpablo@gmail.com>
2022-07-25 21:12:26 -0300

    feat: exemplo de teste como pode ser feito

---
+++ src/components/StarRating/StarRating.spec.tsx
@@ -0,0 +1,18 @@
+// como estamos trabalhando com componenetes precisamos do 'React'
+import React from 'react';
+
+// lib que vai rendenrizar o componente
+import {render} from '@testing-library/react-native';
+
+// componente importado que vai ser renderizado
+import {StarRating} from './StarRating';
+
+// describe - criar um conjunto de teste relacionados
+describe('StarRating', () => {
+  // forma de criar um teste
+  it('the component rendered', () => {
+    // forma de renderizar o componente
+    const {debug} = render(<StarRating rating={{average: 7}} />);
+    debug();
+  });
+});
